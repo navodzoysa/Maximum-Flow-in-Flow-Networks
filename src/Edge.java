7211d9a1774da4452e95ab1845295bfabae13608
Navod Zoysa <navod.zoysa@gmail.com>
2021-03-29 11:07:57 +0530

    added template classes

---
+++ src/Edge.java
@@ -0,0 +1,46 @@
+public class Edge {
+    private int startNode;
+    private int endNode;
+    private int capacity;
+    private int flow;
+
+    public Edge(int startNode, int endNode, int capacity) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.capacity = capacity;
+    }
+
+    public int getStartNode() {
+        return startNode;
+    }
+
+    public int getEndNode() {
+        return endNode;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getFlow() {
+        return flow;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setFlow(int flow) {
+        this.flow = flow;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "startNode=" + startNode +
+                ", endNode=" + endNode +
+                ", capacity=" + capacity +
+                ", flow=" + flow +
+                '}';
+    }
+}
